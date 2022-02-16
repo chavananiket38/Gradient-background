f1a30e57e5c084271616a1cfed8581b657c1cb7d
chavananiket38 <chavananiket38@gmail.com>
2022-02-16 20:56:52 +0530

    Added button to copy text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var css = document.querySelector("h3");
+var css = document.querySelector(".cssText");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 
@@ -7,13 +7,24 @@ var body = document.querySelector(".top");
 var colorPicker = () => {
     body.style.background = "linear-gradient(to right, " + color1.value +", " + color2.value + ")",
     
-    css.textContent = body.style.background;
+    css.value = body.style.background;
 
     body.style.backgroundPosition = "center center";
     body.style.backgroundRepeat = "no-repeat";
     body.style.backgroundAttachment = "fixed";
     body.style.backgroundSize = "cover";
 }
+
+function myFunction() {
+    var copyText = document.querySelector(".cssText");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+    
+  }
 
 color1.addEventListener("input", colorPicker);
 
